2cf9089fd786ef1b6812b408f478673d0ce1feff
Lucas Pigato <lucasgpigato@hotmail.com>
2021-06-03 16:21:30 -0300

    Implementação de design com CSS e atualizações no script

--- script.js
+++ script.js
@@ -3,11 +3,17 @@ var row = 1;
 var entry = document.getElementById("entry");
 entry.addEventListener("click", displayDetails);
 
+	// Update required when add a new var
 function displayDetails() {
 	var departure = document.getElementById("departure").value;
 	var arrival = document.getElementById("arrival").value;
+	var beforeHobbs = document.getElementById("beforeHobbs").value;
+	var afterHobbs = document.getElementById("afterHobbs").value;
+	var beforeFob = document.getElementById("beforeFob").value;
+	var afterFob = document.getElementById("afterFob").value;
 	
-	if(!departure || !arrival) {
+	// Update the if
+	if(!departure || !arrival || !beforeHobbs || !afterHobbs || !beforeFob || !afterFob) {
 		alert("Please fill all the boxes");
 		return;
 	}
@@ -16,11 +22,21 @@ function displayDetails() {
 	
 	var newRow = display.insertRow(row);
 	
+	// Test required. Verify if is required to add a new var cell
 	var cell1 = newRow.insertCell(0);
 	var cell2 = newRow.insertCell(1);
+	var cell3 = newRow.insertCell(2);
+	var cell4 = newRow.insertCell(3);
+	var cell5 = newRow.insertCell(4);
+	var cell6 = newRow.insertCell(5);
 	
+	// Test required. Verify if is required to add a new cell for each input field
 	cell1.innerHTML = departure;
 	cell2.innerHTML = arrival;
+	cell3.innerHTML = beforeHobbs;
+	cell4.innerHTML = afterHobbs;
+	cell5.innerHTML = beforeFob;
+	cell6.innerHTML = afterFob;
 	
 	row++;
 }
